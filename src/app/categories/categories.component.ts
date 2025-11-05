ced9ec3726b88d836fc51d0795f1cf19f9790b9d
Ahmed Mohammed <ahmed.mohammed3588@gmail.com>
2025-11-05 19:24:56 +0200

    Update Category Service, Add categoryDetails

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../services/category.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
-  templateUrl: './categories.component.html'
+  imports: [AsyncPipe, RouterLink],
+  templateUrl: './categories.component.html',
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories$!: Observable<any>;
 
+  constructor(private categoryService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+  }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +9,12 @@ import { Observable, tap } from 'rxjs';
 export class CategoriesService {
   private readonly API_URL = 'https://ecommerce.routemisr.com/api/v1';
   constructor(private http: HttpClient) {}
+
+  getAllCategories(): Observable<any> {
+    return this.http.get(`${this.API_URL}/categories`);
+  }
+
+  getCategoryById(id: string): Observable<any> {
+    return this.http.get(`${this.API_URL}/categories/${id}`);
+  }
 }
